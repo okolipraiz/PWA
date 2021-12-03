ce96f0f5d4eddd44105df0d23dd42e3d6ec1377f
okoli praiz <okolipraiz.com>
2021-12-03 09:56:11 +0100

    Error in SW

--- sw.js
+++ sw.js
@@ -1,14 +1,37 @@
- // install event
-self.addEventListener('install', evt => {
-    console.log('service worker installed');
-  });
-  
-  // activate event
-self.addEventListener('activate', evt => {
-    console.log('service worker activated');
-  });
+const staticCacheName = 'site-static';
+const assets = [
+  "./",
+  "./index.html",
+  "./js/app.js",
+  "/js/ui.js",
+  "./css/styles.css",
+  "./manifest.json",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
 
-// fetch events
-self.addEventListener('fetch', evt =>{
-    // console.log('fetch event', evt);
-})
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
